174e23057366fc6060b931d0cec45089e08e6b6c
Michael Zach <tuk55360@temple.edu>
2025-01-15 13:47:16 -0500

    fixed file to run correctly. runs fine on server and locally

--- random.c
+++ random.c
@@ -6,22 +6,6 @@
 #define MINIMUM 65
 #define MAXIMUM 90
 
-int randchar();
-
-int main()
-{
-	int a;
-
-	/* seed the randomizer */
-	srand( (unsigned)time(NULL) );
-
-	printf("Today's random word: ");
-	for(a=0;a<7;a++)
-		putchar( randchar() );
-	putchar('\n');
-
-	return(0);
-}
 
 int randchar(){
 
